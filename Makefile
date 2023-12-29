64ca194f8c81f196c80fbe378ccdd24a8cbe4c7c
Diogo Lee <81101100+diogoleetao@users.noreply.github.com>
2023-12-29 16:59:40 +0000

    wasps movement
    as wasps já têm movimento e prints corretos. tem colisões e reduz os pontos quando é o lizard a mexer, falta meter redução de pontos quando são as wasps

--- Makefile
+++ Makefile
@@ -1,15 +1,17 @@
 CC = gcc
 CFLAGS = -std=gnu99 -Wall -Wextra -g 
 SRC_DIR = ./src/
-all: lizard-client roaches-client server display-app
+all: lizard-client roaches-client wasp-client server display-app
 	mkdir bin
 	mkdir bin/server
 	mkdir bin/clients
 	mkdir bin/remote-display
 	mkdir bin/clients/lizard
 	mkdir bin/clients/cockroach
+	mkdir bin/clients/wasp
 	mv lizard-client bin/clients/lizard/
 	mv roaches-client bin/clients/cockroach/
+	mv wasp-client bin/clients/wasp/
 	mv display-app bin/remote-display/
 	mv server bin/server/
 lizard-client: $(SRC_DIR)lizard-client.c $(SRC_DIR)remote-char.h
@@ -17,6 +19,9 @@ lizard-client: $(SRC_DIR)lizard-client.c $(SRC_DIR)remote-char.h
 
 roaches-client: $(SRC_DIR)roaches-client.c $(SRC_DIR)remote-char.h
 	$(CC) $(CFLAGS) -o roaches-client $(SRC_DIR)roaches-client.c $(SRC_DIR)auxfuncs.c -lncurses -lzmq -lm
+
+wasp-client: $(SRC_DIR)wasp-client.c $(SRC_DIR)remote-char.h
+	$(CC) $(CFLAGS) -o wasp-client $(SRC_DIR)wasp-client.c $(SRC_DIR)auxfuncs.c -lncurses -lzmq -lm
 
 server: $(SRC_DIR)server.c $(SRC_DIR)remote-char.h
 	$(CC) $(CFLAGS) -o server $(SRC_DIR)server.c $(SRC_DIR)auxfuncs.c -lncurses -lzmq -lm
---
+++ roaches-client.c
@@ -0,0 +1,122 @@
+#include <zmq.h>
+#include <ncurses.h>
+#include "remote-char.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{	 
+    srand((unsigned int) time(NULL));
+
+    if (argc != 3) {
+        printf("Wrong number number of arguments\n");
+        return 1;
+    }
+
+    char *ip = argv[1];
+    char *port1 = argv[2];
+
+    if(!Is_ValidIPv4(ip) || !Is_ValidPort(port1)){
+        printf("ERROR[000]: Incorrect Host-Server data. Check host IPv4 and TCP ports.");
+        exit(1);
+    }
+
+    char *candidate1 = (char*) calloc (1, sizeof(char)*(strlen("tcp://")+strlen(ip) + 1 + strlen(port1) + 1));
+    strcat(candidate1, "tcp://");
+    strcat(candidate1, ip);
+    strcat(candidate1, ":");
+    strcat(candidate1, port1);
+   
+    void *context = zmq_ctx_new ();
+    void *requester = zmq_socket (context, ZMQ_REQ);
+    zmq_connect (requester, candidate1);
+
+    // read number of cockroaches from the user
+    int ncock;
+    do {
+        printf("How many cockroaches (1-10)?: ");
+        if (scanf("%d", &ncock) != 1) {
+            printf("Invalid input. Please enter a number between 1 and 10.\n");
+            while (getchar() != '\n'); // Clear input buffer
+            continue;
+        }
+    } while (ncock < 1 || ncock > 10);
+
+    char password[50];
+    for(int i = 0; i < 50; i++){
+        password[i] = (char) random()%94 + 32;
+    }
+    password[49]='\0';
+
+    // send connection message
+    remote_char_t m;
+    m.msg_type = 3;
+    m.ncock = ncock;
+    strcpy(m.password, password);
+    zmq_send (requester, &m, sizeof(m), 0);
+    zmq_recv (requester, &m, sizeof(m), 0);
+
+    //board already full of cockroaches
+    if(m.ncock == 0){
+        printf("Tamos cheios :/\n");
+        return 0;
+    }
+
+    //load the message information, m.ch is the first position
+    m.msg_type = 4;
+
+    int sleep_delay;
+    direction_t direction;
+    int n = 0;
+    int move;
+    while (1)
+    {
+        n++;
+        sleep_delay = random()%700000;
+        usleep(sleep_delay);
+
+        for(int  i = 0; i < ncock; i++){
+            move = random()%2;
+            if(move == 1){
+            direction = random()%4;
+
+            switch (direction)
+            {
+            case LEFT:
+                mvprintw(0,0,"%d Going Left   \n", n);
+                m.cockdir[i] = direction;
+                break;
+            case RIGHT:
+                mvprintw(0,0,"%d Going Right   \n", n);
+                m.cockdir[i] = direction;
+                break;
+            case DOWN:
+                mvprintw(0,0,"%d Going Down   \n", n);
+                m.cockdir[i] = direction;
+                break;
+            case UP:
+                mvprintw(0,0,"%d Going Up    \n", n);
+                m.cockdir[i] = direction;
+                break;
+            }
+            }else{
+                m.cockdir[i] = 5;
+            }
+        }
+        refresh();
+        //send the movement message
+        zmq_send (requester, &m, sizeof(m), 0);
+        zmq_recv (requester, &m, sizeof(m), 0);
+    }
+
+    zmq_close (requester);
+    zmq_ctx_destroy (context);
+    free(candidate1);
+	return 0;
+}
